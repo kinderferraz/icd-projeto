b6fd7b3c712474a2cc80e949ac568b351c5b9d50
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2022-11-29 14:24:12 -0300

    Update and rename shinydashboard.R to ui.R

--- src/ui.R
+++ src/ui.R
@@ -1,21 +1,8 @@
-ui.R
 install.packages("shinydashboard")
 library(shinydashboard)
-
 
 dashboardPage(
   dashboardHeader(),
   dashboardSidebar(),
   dashboardBody()
 )
-
-
-app
-
-
-library(shinydashboard)
-
-
-
-
-shinyApp(ui, server)
