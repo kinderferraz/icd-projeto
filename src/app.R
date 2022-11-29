fd0a281c749eff5523674e901061f4e69a9e8b5c
Marcelo Junior <j.olimpio.marcelo@gmail.com>
2022-11-29 14:23:25 -0300

    Update and rename ui.R to app.R

--- src/app.R
+++ src/app.R
@@ -1,3 +1,5 @@
+library(shinydashboard)
+
 ui <- dashboardPage (
   dashboardHeader(title = "Meu Dashboard", 
                   dropdownMenu(type = "messages", #escolha entre “messages”, “notifications”, “tasks”
@@ -59,3 +61,24 @@ ui <- dashboardPage (
     )
   ),
 )
+
+server <- function(input, output) {
+  histdata<-apenas_alugueis$Price
+  
+  output$plot1 <- renderPlot({
+    data <- histdata[seq_len(input$slider)]
+    hist(data, main = paste("Histograma de Preços"), xlab = "Preço", ylab = "Frequência")
+    
+  })
+  
+  output$plot2 <- renderPlot({
+    
+    # Renderizar o barplot
+    barplot(pollution_records[,input$select], 
+            main=input$select,
+            ylab="Valor",
+            xlab="Ano")
+  }) 
+}
+
+shinyApp(ui, server)
